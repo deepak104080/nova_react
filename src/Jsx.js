b1cfa1ca03479e722523fe23fb5b306c7319522e
deepak104080 <deepak104080@gmail.com>
2023-01-12 23:20:36 +0530

    12th Jan 2023 - JSX

--- src/Jsx.js
+++ src/Jsx.js
@@ -1,14 +1,55 @@
 import React, {Fragment} from 'react';
+import './App.css';
+import imageBg from './assets/images/image1.png';
 
 function Jsx() {
     let name = 'NS';
     let num1 = 5;
     let num2 = 6;
+
+    let status = 0;
+    let statusElse = false;
+
+    let CarArray = ['Tata', 'Hyundai', 'Suzuki', 'Mahindra', 'Honda', 'Scoda', 'Range Rover'];
+    let CarArray2 = [{name: 'Tata'}, {name: 'Hyundai'}, {name: 'Suzuki'}, {name: 'Mahindra'}];
+
     return(
         <>
-            <span>My first component.</span>
+            <div style={{'background': '#ff0000'}}>My first component.</div>
             <span>{name}</span>
             <span>Sum = {num1+num2}</span>
+
+            {(status + 5 === 0) && (<div>If condition is true</div>)}
+
+            {
+                (statusElse) ? (<div>True Part</div>) : (<div>False Part</div>) 
+            }
+
+            <div>List of Car Companies</div>
+            <ul id="list">
+                
+                {
+                    CarArray.map((temp, index) => (
+                        <li key={temp+index} class="listitem">{temp}</li>
+                    ))
+                }
+            </ul>
+            <div>List of Car Companies 2</div>
+            <ul>
+                
+                {
+                    CarArray2.map((temp, index) => (
+                        <li key={temp.name+index} className="listitem">{temp.name}</li>
+                    ))
+                }
+            </ul>
+            <br></br>
+            <div>
+                <img src={imageBg} alt="Image1" />
+            </div>
+            {/* <div>
+                <img src='https://images.prismic.io/frameworkmarketplace/46cbf974-cdff-4cd8-b761-8b4a3343f6c4_FW-chromebook-homepage-carousel.png'/>
+            </div> */}
         </>
 
 /* <Fragment>
@@ -29,3 +70,6 @@ export default Jsx;
 //     return a;
 // }
 
+// React Conditional
+// if - && Operator
+// if else - ternary operator
